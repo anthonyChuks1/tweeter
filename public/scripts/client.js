71251853733426c1e0116ab9411aca91f02b657f
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-08-09 21:09:01 -0400

    added an ajax post for the form submit that sends the serialized data to /tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,6 +3,7 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
 // Fake data taken from initial-tweets.json
 const data = [
   {
@@ -28,49 +29,78 @@ const data = [
     created_at: 1461113959088,
   },
 ];
+
+// Function to create a tweet element from a tweet object
 const createTweetElement = function (tweet) {
   const { user, content, created_at } = tweet;
-  /* Your code for creating the tweet element */
+
+  // Constructing the tweet element using template literals
   let $tweet = ` 
         <article class="tweet">
           <header>
             <!-- User avatar and name go here -->
-             <div>
-            <img src=${user.avatars} alt="User avatar" class="avatar">
-            <p class="username">${user.name}</p>
+            <div>
+              <img src=${user.avatars} alt="User avatar" class="avatar">
+              <p class="username">${user.name}</p>
             </div>
-            <p>${user.handle} </p>
+            <p>${user.handle}</p>
           </header>
           <p class="tweet-content">
             <!-- This is where the tweet text will go -->
             ${content.text}
           </p>
           <footer>
-            <!-- Action icons go here -->
-             <div>${created_at}</div>
-             <div>
-            <i class="fa fa-reply"></i>
-            <i class="fa fa-retweet"></i>
-            <i class="fa fa-heart"></i>
+            <!-- Timestamp and action icons go here -->
+            <div>${new Date(created_at).toLocaleString()}</div>
+            <div>
+              <i class="fa fa-reply"></i>
+              <i class="fa fa-retweet"></i>
+              <i class="fa fa-heart"></i>
             </div>
           </footer>
         </article>
       `;
-  // ...
   return $tweet;
 };
 
+// Function to render tweets on the page
 const renderTweets = function (tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
+  // Log the tweets being rendered
   console.log("Rendering tweets:", tweets);
+
+  // Loop through each tweet
   for (let tweet of tweets) {
+    // Create a tweet element
     const tweetElement = createTweetElement(tweet);
+
+    // Append the tweet element to the tweets container
     $("#tweets-container").prepend(tweetElement);
   }
 };
 
-$(document).ready(function () {
+// Document ready function to ensure DOM is fully loaded
+$(document).ready(function() {
+  // Render initial tweets
   renderTweets(data);
+
+  // Event listener for new tweet submission
+  $('form').on('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    
+    const serializedData = $(this).serialize(); // Serialize form data
+
+    // AJAX POST request to submit the new tweet
+    $.ajax({
+      type: 'POST',
+      url: '/tweets',
+      data: serializedData,
+      success: function(response) {
+        console.log('Tweet posted successfully:', response);
+        console.log(serializedData);
+      },
+      error: function(error) {
+        console.error('Error posting tweet:', error);
+      }
+    });
+  });
 });
